72990fff7785216dcefa92a2b7b92014f55c5a2c
anejjar <anejjarlhouciane@gmail.com>
2020-01-04 19:18:29 +0100

    add migration table

---
+++ database/migrations/2020_01_04_181211_create_avis_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAvisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('avis', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->longText('avis');
+            $table->date('date');
+            $table->int('stars');
+            $table->char('title',200);
+            $table->int('is_public');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('avis');
+    }
+}
